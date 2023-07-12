f4461edcf9fcc457b310b091e0b452285a9a6de6
Frederico Tavares <fredericotp@hotmail.com>
2023-07-12 14:11:50 -0300

    Feat: Requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    produto_id = "1"
+    nome = "Biscoito"
+    marca = "Xablau"
+    fabricacao = "2022-04-04"
+    validade = "2024-08-08"
+    serial = "123"
+    instrucao = "Ambiente iluminado"
+
+    produto = Product(
+        produto_id, nome, marca, fabricacao, validade, serial, instrucao
+    )
+
+    assert produto.id == produto_id
+    assert produto.product_name == nome
+    assert produto.company_name == marca
+    assert produto.manufacturing_date == fabricacao
+    assert produto.expiration_date == validade
+    assert produto.serial_number == serial
+    assert produto.storage_instructions == instrucao
