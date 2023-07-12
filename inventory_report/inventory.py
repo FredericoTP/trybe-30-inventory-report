b85c96f088912fa5423f75bc5109519ad6d1dab2
Frederico Tavares <fredericotp@hotmail.com>
2023-07-12 16:02:13 -0300

    Feat: Requisito 5 ~ dando KeyError: 'data' ~ Arrumar

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from inventory_report.product import Product
+from typing import Optional
+
+
 class Inventory:
-    pass
+    def __init__(self, data: Optional[list[Product]] = None) -> None:
+        self._data = data if data else []
+
+    @property
+    def data(self) -> list[Product]:
+        return self._data.copy()
+
+    def add_data(self, products_list: list[Product]) -> None:
+        self._data.extend(products_list)
