b06e092d643d2b633ba6d2a3cd7304860e5d6d69
Frederico Tavares <fredericotp@hotmail.com>
2023-07-12 14:19:04 -0300

    Feat: Requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,27 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    produto_id = "1"
+    nome = "Biscoito"
+    marca = "Xablau"
+    fabricacao = "2022-04-04"
+    validade = "2024-08-08"
+    serial = "123"
+    instrucao = "Ambiente iluminado"
+
+    produto = Product(
+        produto_id, nome, marca, fabricacao, validade, serial, instrucao
+    )
+
+    report = str(produto)
+
+    assert report == (
+        f"The product {produto_id} - {nome} "
+        f"with serial number {serial} "
+        f"manufactured on {fabricacao} "
+        f"by the company {marca} "
+        f"valid until {validade} "
+        f"must be stored according to the following instructions: "
+        f"{instrucao}."
+    )
