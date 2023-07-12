f95769da03793956eaf16079bea140aa28ff0459
Frederico Tavares <fredericotp@hotmail.com>
2023-07-12 20:51:46 -0300

    Feat: Refatoração input_handler

--- inventory_report/cli/input_handler.py
+++ inventory_report/cli/input_handler.py
@@ -10,55 +10,26 @@ def process_report_request(file_paths: List[str], report_type: str) -> str:
     if report_type not in types:
         raise ValueError("Report type is invalid.")
 
+    inventory = Inventory()
+    report = None
+
+    for file in file_paths:
+        process_returns(file, inventory)
+
     if report_type == "simple":
-        response = ""
-        inventory = Inventory()
+        report = SimpleReport()
 
-        for file in file_paths:
-            simple = process_returns_simple(file, inventory)
-            response += simple
-
-        return response
     else:
-        response = ""
-        inventory = Inventory()
+        report = CompleteReport()
 
-        for file in file_paths:
-            complete = process_returns_complete(file, inventory)
-            response += complete
-
-        return response
+    report.add_inventory(inventory)
+    return report.generate()
 
 
-def process_returns_simple(file_path: str, inventory: Inventory) -> str:
+def process_returns(file_path: str, inventory: Inventory) -> None:
     if file_path.endswith(".json"):
         json_data = JsonImporter(file_path)
         inventory.add_data(json_data.import_data())
-        report = SimpleReport()
-        report.add_inventory(inventory)
-        return report.generate()
     elif file_path.endswith(".csv"):
         csv_data = CsvImporter(file_path)
         inventory.add_data(csv_data.import_data())
-        report = SimpleReport()
-        report.add_inventory(inventory)
-        return report.generate()
-    else:
-        return ""
-
-
-def process_returns_complete(file_path: str, inventory: Inventory) -> str:
-    if file_path.endswith(".json"):
-        json_data = JsonImporter(file_path)
-        inventory.add_data(json_data.import_data())
-        report = CompleteReport()
-        report.add_inventory(inventory)
-        return report.generate()
-    elif file_path.endswith(".csv"):
-        csv_data = CsvImporter(file_path)
-        inventory.add_data(csv_data.import_data())
-        report = CompleteReport()
-        report.add_inventory(inventory)
-        return report.generate()
-    else:
-        return ""
