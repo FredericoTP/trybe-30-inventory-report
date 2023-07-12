5377e0a2e1b1f4018c13982c71ee6125c481ac09
Frederico Tavares <fredericotp@hotmail.com>
2023-07-12 14:41:28 -0300

    Feat: Requisito 4

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,6 +1,7 @@
 from typing import Dict, Type
 from inventory_report.product import Product
 from abc import ABC, abstractmethod
+import json
 
 
 class Importer(ABC):
@@ -12,8 +13,27 @@ class Importer(ABC):
         ...
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> list[Product]:
+        data = []
+
+        with open(self.path) as file:
+            products = json.load(file)
+
+        for item in products:
+            data.append(
+                Product(
+                    item["id"],
+                    item["product_name"],
+                    item["company_name"],
+                    item["manufacturing_date"],
+                    item["expiration_date"],
+                    item["serial_number"],
+                    item["storage_instructions"],
+                )
+            )
+
+        return data
 
 
 class CsvImporter:
